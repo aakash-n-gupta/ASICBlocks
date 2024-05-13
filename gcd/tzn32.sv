6deb7d51e14e8bb901823be8e4fe3f483fd5be34
Aakash <aakashnareshgupta@gmail.com>
2024-05-13 20:57:04 +0530

    Add module to count trailing zeros

--- gcd/tzn32.sv
+++ gcd/tzn32.sv
@@ -11,78 +11,48 @@ module tzn32 #(WIDTH = 32) (
 
     logic [$clog2(WIDTH):0] numz;
 
+    assign numz_o = numz;
+
     always_comb begin
         numz = '0;
-        if ((a_i & 32'h0000_0001) == 0)
-            numz = 0;
-        else if ((a_i & 32'h0000_0011) == 0)
-            numz = 2;
-        else if ((a_i & 32'h0000_0007) == 0)
-            numz = 3;
-        else if ((a_i & 32'h0000_000f) == 0)
-            numz = 4;
-        else if ((a_i & 32'h0000_001f) == 0)
-            numz = 5;
-        else if ((a_i & 32'h0000_003f) == 0)
-            numz = 6;
-        else if ((a_i & 32'h0000_007f) == 0)
-            numz = 7;
-        else if ((a_i & 32'h0000_00ff) == 0)
-            numz = 8;
-
-
-        else if ((a_i & 32'h0000_01ff) == 0)
-            numz = 9;
-        else if ((a_i & 32'h0000_03ff) == 0)
-            numz = 10;
-        else if ((a_i & 32'h0000_07ff) == 0)
-            numz = 11;
-        else if ((a_i & 32'h0000_0fff) == 0)
-            numz = 12;
-        else if ((a_i & 32'h0000_1fff) == 0)
-            numz = 13;
-        else if ((a_i & 32'h0000_3fff) == 0)
-            numz = 14;
-        else if ((a_i & 32'h0000_7fff) == 0)
-            numz = 15;
-        else if ((a_i & 32'h0000_ffff) == 0)
-            numz = 16;
-
-        else if ((a_i & 32'h0001_ffff) == 0)
-            numz = 17;
-        else if ((a_i & 32'h0003_ffff) == 0)
-            numz = 18;
-        else if ((a_i & 32'h0007_ffff) == 0)
-            numz = 19;
-        else if ((a_i & 32'h000f_ffff) == 0)
-            numz = 20;
-        else if ((a_i & 32'h001f_ffff) == 0)
-            numz = 21;
-        else if ((a_i & 32'h003f_ffff) == 0)
-            numz = 22;
-        else if ((a_i & 32'h007f_ffff) == 0)
-            numz = 23;
-        else if ((a_i & 32'h00ff_ffff) == 0)
-            numz = 24;
-
-        else if ((a_i & 32'h01ff_ffff) == 0)
-            numz = 25;
-        else if ((a_i & 32'h03ff_ffff) == 0)
-            numz = 26;
-        else if ((a_i & 32'h07ff_ffff) == 0)
-            numz = 27;
-        else if ((a_i & 32'h0fff_ffff) == 0)
-            numz = 28;
-        else if ((a_i & 32'h1fff_ffff) == 0)
-            numz = 29;
-        else if ((a_i & 32'h3fff_ffff) == 0)
-            numz = 30;
-        else if ((a_i & 32'h7fff_ffff) == 0)
-            numz = 31;
-        else if ((a_i & 32'hffff_ffff) == 0)
+        if(a_i == '0)
             numz = 32;
-    end
+        casex (a_i)
+            32'hxxxx_xxx1: numz = 1;
+            32'hxxxx_xxx4: numz = 2;
+            32'hxxxx_xxx8: numz = 3;
+            32'hxxxx_xx10: numz = 4;
+            32'hxxxx_xx20: numz = 5;
+            32'hxxxx_xx40: numz = 6;
+            32'hxxxx_xx80: numz = 7;
 
-    assign numz_o = numz;
-endmodule
+            32'hxxxx_x100: numz = 8;
+            32'hxxxx_x200: numz = 9;
+            32'hxxxx_x400: numz = 10;
+            32'hxxxx_x800: numz = 11;
+            32'hxxxx_1000: numz = 12;
+            32'hxxxx_2000: numz = 13;
+            32'hxxxx_4000: numz = 14;
+            32'hxxxx_8000: numz = 15;
+
+            32'hxxx1_0000: numz = 16;
+            32'hxxx2_0000: numz = 17;
+            32'hxxx4_0000: numz = 18;
+            32'hxxx8_0000: numz = 19;
+            32'hxx10_0000: numz = 20;
+            32'hxx20_0000: numz = 21;
+            32'hxx40_0000: numz = 22;
+            32'hxx80_0000: numz = 23;
+
+            32'hx100_0000: numz = 24;
+            32'hx200_0000: numz = 25;
+            32'hx400_0000: numz = 26;
+            32'hx800_0000: numz = 27;
+            32'h1000_0000: numz = 28;
+            32'h2000_0000: numz = 29;
+            32'h4000_0000: numz = 30;
+            32'h8000_0000: numz = 31;
+        endcase
+    end
+    endmodule
 
