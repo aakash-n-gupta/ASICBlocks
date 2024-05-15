733ec0f9bb664e2d5f09491f18c4042007e6d963
Aakash <aakashnareshgupta@gmail.com>
2024-05-16 01:23:12 +0530

    Minor testbench enhancements
    - make multiplier 32 bit: works!
    - add task for multiplier input
    - add cycle count for performance evaluation

--- simple_mult/test/mult_tb.sv
+++ simple_mult/test/mult_tb.sv
@@ -2,7 +2,9 @@
 
 module mult_tb;
 
-    parameter XLEN = 16;
+    parameter XLEN = 32;
+    parameter TEST_CYCLES = 10000;
+    int cycles = 0;
 
     logic clk_i, resetn_i, ld_input_i;
     logic [XLEN-1:0] a_i;
@@ -16,6 +18,8 @@ module mult_tb;
 
 
     always #5 clk_i = ~clk_i;
+    always #10 cycles = cycles + 1;
+
     initial begin
         $dumpfile("./mult.vcd");
         $dumpvars(0, mult_tb);
@@ -24,8 +28,8 @@ module mult_tb;
     task multiplier_input(int a, int b);
         resetn_i = 1;
         ld_input_i = '1;
-        a_i = 17;
-        b_i = 5;
+        a_i = a;
+        b_i = b;
         #10;
         a_i = 0;
         b_i = 0;
@@ -33,7 +37,7 @@ module mult_tb;
     endtask
 
     initial begin
-        $monitor("Monitor backpressure signals | valid = %b || ready = %b\n", valid_o, ready_o);
+        $monitor("Monitor backpressure signals | valid = %d || ready = %d", valid_o, ready_o);
         $monitor("Product = %d", product_o);
         #1;
         clk_i = 0;
@@ -43,26 +47,26 @@ module mult_tb;
         b_i = 0;
         #20;
         // start the datapath operations
-        resetn_i = 1;
-        ld_input_i = '1;
-        a_i = 17;
-        b_i = 5;
-        #10;
-        a_i = 0;
-        b_i = 0;
-        ld_input_i = 0;
-        #40;
-        // $display("Product = %d", product_o);
+        $display("cycle count before Mult input: %d", cycles);
+        multiplier_input(17090, 1119);
+        $display("cycle count after Mult input : %d", cycles);
 
-        // Do some things in meanwhile
-        for (int i = 0; i < 16; i++)
-        begin
-            // $display("Product = %d", product_o);
+        for (int i = 0; i < TEST_CYCLES; i++ )  begin
             #10;
+            if(valid_o)
+                $display("cycle count: %d when Multiplication Complete", cycles);
         end
 
+        $display("cycle count before Mult input: %d", cycles);
+        multiplier_input(1701, 199);
+        $display("cycle count after Mult input : %d", cycles);
 
-        #100;
+        for (int i = 0; i < TEST_CYCLES; i++ )  begin
+            #10;
+            if(valid_o)
+                $display("cycle count: %d when Multiplication Complete", cycles);
+        end
+
         $finish;
 
     end
