d3e698556744f331649fcd7899565978ada2f6cb
Aakash <aakashnareshgupta@gmail.com>
2024-05-24 17:58:51 +0530

    Improve GCD datapath
    the datapath now right shift after each itteration of subtraction
    to reduce total cycles needed for final calculation

--- gcd/gcd_datapath.sv
+++ gcd/gcd_datapath.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module gcd_datapath #( parameter XLEN = 32)
+module gcd_datapath #( parameter XLEN = 16)
 (
     // clock and reset from top
     input clk,
@@ -16,22 +16,28 @@ module gcd_datapath #( parameter XLEN = 32)
 
     // outputs to control
     output eq,
-    output altb,
 
     // outputs to top
     output [XLEN-1:0] gcd
 );
-
     import gcd_pkg::*;
+
+    logic sink_a;
+    logic sink_b;
+
+    // Wires to save a and b
+    logic [XLEN-1:0] a_mux;
+    logic [XLEN-1:0] b_mux;
+    logic [XLEN-1:0] a_sub;
+    logic [XLEN-1:0] b_sub;
 
     // registers for a and b
     logic [XLEN-1:0] a_r;
     logic [XLEN-1:0] b_r;
-    logic [XLEN-1:0] gcd_w;
 
     // wires for comparators
     logic eq_w;
-    logic altb_w;
+    logic [XLEN-1:0] gcd_w;
 
     logic [$clog2(XLEN)-1:0] shamt_a;
     logic [$clog2(XLEN)-1:0] shamt_b;
@@ -41,66 +47,80 @@ module gcd_datapath #( parameter XLEN = 32)
     logic [$clog2(XLEN)-1:0] final_shamt;
     logic [$clog2(XLEN)-1:0] gcd_shamt;
 
-
-    logic sink_a;
-    logic sink_b;
-
     // muxA and muxB to select value to store
+    always_comb begin
+        a_mux = a_r;
+        if(ld_i)
+            a_mux = a_i;
+        case (state)
+            // LOAD: a_mux = a_i;
+            ODD: a_mux = (a_r >> shamt_a);
+            COMPUTE: a_mux = a_sub;
+        endcase
+    end
+
     always_ff @(posedge clk) begin
         if (!resetn)
             a_r <= '0;
-        else begin
-            if (ld_i)
-                a_r <= a_i;
-            else if (state == ODD)
-                a_r <= a_r >> shamt_a;
-            else if (state == DIVA)
-                a_r <= (a_r - b_r);
-        end
+        else
+            a_r <= a_mux;
     end
 
-    tzn16 #(.WIDTH(XLEN)) reduceA(.a_i(a_r), .numz_o(shamt_a), .all_zeros_o(sink_a));
+    always_comb begin
+        b_mux = b_r;
+        if(ld_i)
+            b_mux = b_i;
+        case (state)
+            // LOAD: b_mux = b_i;
+            ODD: b_mux = (b_r >> shamt_b);
+            COMPUTE: b_mux = b_sub;
+        endcase
+    end
 
     always_ff @(posedge clk) begin
         if (!resetn)
             b_r <= '0;
-        else begin
-            if (ld_i)
-                b_r <= b_i;
-            else if (state == ODD)
-                b_r <= b_r >> shamt_b;
-            else if (state == DIVB)
-                b_r <= (b_r - a_r);
-            else
-                b_r <= b_r;
-        end
+        else
+            b_r <= b_mux;
     end
 
+    // Subtractor for X - Y, where X is always greater than Y
+    // inputs: a_r, b_r
+    // Outputs: a_sub, b_sub,
+    // where a_sub <- (a-b) or (b-a) and b_sub <- b or a , conditionally on a > b
+    subtractor #(.XLEN(XLEN)) sub (.a_i(a_r), .b_i(b_r), .a_sub(a_sub), .b_sub(b_sub));
+
     // reduce a and b to odd values
+    tzn16 #(.WIDTH(XLEN)) reduceA(.a_i(a_r), .numz_o(shamt_a), .all_zeros_o(sink_a));
     tzn16 #(.WIDTH(XLEN)) reduceB(.a_i(b_r), .numz_o(shamt_b), .all_zeros_o(sink_b));
 
     // Comparator
-    assign eq_w   = (a_r == b_r) ? 1'b1 : 1'b0;
-    assign altb_w = (a_r < b_r) ? 1'b1 : 1'b0;
+    assign eq_w   = (b_r == 0) ? 1'b1 : 1'b0;
+
+    // Keep track of previous state
+    logic [2:0] priv_state;
+
+    always_ff @(posedge clk) begin
+        priv_state <= state;
+    end
+
+    always_ff @(posedge clk) begin
+        if (!resetn)
+            gcd_shamt <= 0;
+        else begin
+            gcd_shamt <= gcd_shamt;
+            if (priv_state == LOAD) begin
+                gcd_shamt <= final_shamt;
+            end
+        end
+    end
+
+    assign final_shamt = (shamt_a >= shamt_b) ? shamt_b : shamt_b;
 
     assign gcd_w =  (a_r << gcd_shamt);
     assign gcd = (state == DONE) ? gcd_w : 'Z;
 
-    assign final_shamt = (shamt_a >= shamt_b) ? shamt_b : shamt_b;
-
-    always_ff @(posedge clk)
-    begin
-        if (!resetn)
-            gcd_shamt <= '0;
-        else
-            if(state == ODD)
-                gcd_shamt <= final_shamt;
-            else
-                gcd_shamt <= gcd_shamt;
-    end
-
     assign eq = eq_w;
-    assign altb = altb_w;
 
     // save shift value for final gcd calculation
     // use this as a final shift left to calculate gcd
