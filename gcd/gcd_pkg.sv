1d29f164c12aca3e3a6d07f1b03376e737d9eaf5
Aakash <aakashnareshgupta@gmail.com>
2024-05-24 17:47:48 +0530

    Update state machine to optimized GDC

--- gcd/gcd_pkg.sv
+++ gcd/gcd_pkg.sv
@@ -7,10 +7,8 @@ package gcd_pkg;
     typedef enum logic[2:0] {
         READY   = 3'b111, //7
         LOAD    = 3'b001, //1
-        ODD     = 3'b010, //2
-        CMP     = 3'b100, //4
-        DIVA    = 3'b011, //3
-        DIVB    = 3'b000, //0
+        ODD     = 3'b110, //6
+        COMPUTE = 3'b010, //2
         DONE    = 3'b101  //5
 
     } state_t;
